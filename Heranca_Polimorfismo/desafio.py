19760f30cf64482174f4d19cdcc1effaafdd9a22
profclovesrocha <clovesoficial@gmail.com>
2023-03-03 20:48:43 -0300

    Update desafio.py

--- Heranca_Polimorfismo/desafio.py
+++ Heranca_Polimorfismo/desafio.py
@@ -2,4 +2,10 @@
 # Aula Prática: Herança e Polimorfismo
 # Polimorfismo - Code 1
 # Desafio 
-#Desenvolva um objeto 'Animal' com aplicação dos conceitos de Herança e Polimorfismo.
+# Desenvolva um objeto 'Animal' com aplicação dos conceitos de Herança e Polimorfismo.
+# Reino animal: principais características e classificações
+# - Anfíbios.
+# - Aves.
+# - Mamíferos.
+# - Peixes.
+# - Répteis.
